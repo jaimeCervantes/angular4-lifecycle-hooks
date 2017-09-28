091f2bc80435f34c2aaa0159fe37252e305a5a40
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2017-09-28 16:51:37 -0500

    feat(ts,htm): Completing "All Hooks demo"

--- src/app/all/all-hooks.module.ts
+++ src/app/all/all-hooks.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ParentComponent } from './parent.component';
 import { ChildComponent } from './child.component';
 
 @NgModule({
+  imports: [
+    CommonModule
+  ],
   declarations:[
     ParentComponent,
     ChildComponent
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hola {{name}}</h1>
+  template: `<h1>Hooks del ciclo de vida de un componente</h1>
   <lch-all-parent></lch-all-parent>`,
 })
 export class AppComponent  { name = 'Jaime'; }
